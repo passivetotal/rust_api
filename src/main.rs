b8c187bf89bd1d558ab33b946693cf3b75a0f0ea
Johan Nestaas <johannestaas@gmail.com>
2016-03-28 22:11:44 -0700

    docopt with pdns command

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,41 @@
+extern crate docopt;
+extern crate rustc_serialize;
 extern crate passivetotal;
+
+use docopt::Docopt;
 
 use passivetotal::config;
 use passivetotal::PTClient;
 use passivetotal::constants::Source;
 
+static USAGE: &'static str = "
+Usage: passivetotal pdns <query>
+       passivetotal --help
+       
+       Option:
+            -h, --help      Show this error message
+";
+
+#[derive(Debug, RustcDecodable)]
+struct Args {
+    cmd_pdns: bool,
+    arg_query: String,
+}
+
 fn main() {
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.decode())
+        .unwrap_or_else(|err| err.exit());
     let conf = config::read_config();
     let client = PTClient::from(conf);
-    let response = client.get_pdns("passivetotal.org");
-    println!("PDNS results for passivetotal.org from RiskIQ:");
 
-    for result in response.results {
-        if result.source.contains(&Source::RISKIQ.string()) {
-            println!("{}: {}", result.lastSeen.unwrap(), result.resolve.unwrap());
+    if args.cmd_pdns {
+        let response = client.get_pdns(args.arg_query.as_str());
+        println!("PDNS results for passivetotal.org from RiskIQ:");
+        for result in response.results {
+            if result.source.contains(&Source::RISKIQ.string()) {
+                println!("{}: {}", result.lastSeen.unwrap(), result.resolve.unwrap());
+            }
         }
     }
-
 }
