8439bb9906b733f7d92c1ef1fba1e42c69244acc
Johan Nestaas <johannestaas@gmail.com>
2016-03-30 02:13:44 -0700

    move tests out

--- src/lib.rs
+++ src/lib.rs
@@ -10,13 +10,3 @@ pub mod constants;
 pub mod config;
 pub mod response;
 pub mod client;
-
-#[test]
-fn test_pdns_riskiq() {
-    let conf = config::read_config().unwrap();
-    let client = client::PTClient::from(conf);
-    let response = client.get_pdns("sf.riskiq.net");
-    let results = response.results.unwrap();
-    assert_eq!(results.len(), 2);
-    assert_eq!(results[0].resolve, Some("192.65.247.107".to_string()));
-}
