452dd5d681f714738b68239aff6cbef8320a407b
Johan Nestaas <johannestaas@gmail.com>
2016-03-31 10:28:42 -0700

    docs

---
+++ doc/passivetotal/client/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["ResponseError",""]],"struct":[["PTClient","This is the client exposed to the user for abstracting the passivetotal API."]]});
---
+++ doc/passivetotal/config/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["ConfigError",""]],"fn":[["read_config",""]],"struct":[["Config",""]]});
